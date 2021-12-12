bb4c99371f979cb9442f6ebe14b0f73e86c662df
FelipeSSDev <felipedevbrasil@gmail.com>
2021-12-12 10:23:56 -0300

    fix: Add filters/validation to create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MaxLength(75)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string;
 }
