397e581b5002b4b121352d016c3aa9a5be50dd13
FelipeSSDev <felipedevbrasil@gmail.com>
2021-12-12 18:32:27 -0300

    fix: Move typeorm config to ormconfig.js file

---
+++ ormconfig.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  migrationsTableName: 'custom_migration_table',
+  migrations: ['src/migrations/*.js'],
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  subscribers: ['dist/subscribers/**/*{.js,.ts}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: parseInt(process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
